482600674728fd84d0856d651805c84fe9f3ec5e
abhishek048 <abhishekchaurasiya8299@gmail.com>
2025-07-24 16:42:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="navbar">
+        <h1>Abhishek</h1>
+        <div className="navelink">
+             <a href="">Home</a>
+             <a href="">About</a>
+             <a href="">Contact</a>
+             <a href="">Service</a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
